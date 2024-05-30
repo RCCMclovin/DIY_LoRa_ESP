03b0ec72ae2f4030ffec8c2e2ee708371ad91a8f
Rafael Carvalho <rafacien.rc@gmail.com>
2024-05-30 15:46:36 -0400

    Add SD card support

--- LoRaSender/LoRaSender.ino
+++ LoRaSender/LoRaSender.ino
@@ -1,26 +1,52 @@
 #include <SPI.h>
+#include <SD.h>
 #include <LoRa.h>
 
 //LoRa Pins
-#define csPin 15
-#define resetPin D1
-#define irqPin D0
+#define csPin 3
+#define resetPin 1
+#define irqPin 2
 
-
+//SD Pin
+#define CS 7
 
 //SPI Interface Pins
-#define SCK D5
-#define MISO D6
-#define MOSI D7
+#define SCK 4
+#define MISO 5
+#define MOSI 6
 
 
 
 int counter = 0;
 
 void setup() {
+  //pinMode(csPin, OUTPUT);
+  //pinMode(CS, OUTPUT);
+  
   Serial.begin(115200);
   while (!Serial);
+  Serial.println("");
 
+  ///*
+
+  //digitalWrite(CS,HIGH);
+  //digitalWrite(csPin,LOW);
+
+  Serial.print("Inicializando o cartão SD...");
+  SPI.begin(SCK,MISO,MOSI,CS);
+  if (!SD.begin(CS)) {
+    Serial.println("Falha, verifique se o cartão está presente.");
+    //Early return
+    return;
+  }
+  Serial.println("Cartão inicializado.");
+
+  //digitalWrite(CS,LOW);
+  //digitalWrite(csPin,HIGH);
+
+  //*/
+
+  ///*
   Serial.println("LoRa Sender");
   LoRa.setPins(csPin, resetPin, irqPin);
 
@@ -30,11 +56,13 @@ void setup() {
   }
 
   //Set LoRa Params
-  LoRa.setSpreadingFactor(7);
+  LoRa.setSpreadingFactor(9);
   LoRa.setCodingRate4(5);
   LoRa.setSignalBandwidth(125E3);
   LoRa.setPreambleLength(8);
   LoRa.setTxPower(20, PA_OUTPUT_PA_BOOST_PIN);
+
+  //*/
 
 }
 
@@ -42,14 +70,37 @@ void loop() {
   Serial.print("Sending packet: ");
   Serial.println(counter);
 
+  ///*
+
+  //digitalWrite(CS,HIGH);
+  //digitalWrite(csPin, LOW);
+
+  File dataFile = SD.open("/LOG.txt", FILE_APPEND);
+  if (dataFile) { //Verify if file was oppened correctly
+    Serial.println("O arquivo foi aberto com sucesso.");
+      dataFile.println(counter);
+      dataFile.close();
+  }
+  else {
+    Serial.println("Falha ao abrir o arquivo LOG.txt");
+  }
+
+  //digitalWrite(CS,LOW);
+  //digitalWrite(csPin, HIGH);
+
+  //*/
+
+  ///*
   // send packet
   LoRa.beginPacket();
-
+  Serial.print(".");
   LoRa.print("hello ");
   LoRa.print(counter);
-
+  Serial.print(".");
   LoRa.endPacket();
+  Serial.println(".");
 
+  //*/
   counter++;
 
   delay(2000);
--- README.md
+++ README.md
@@ -1,7 +1,8 @@
-# DIY_LoRa_ESP V1.0
-Self build ESP8266 + LoRa device for environment monitoring on the Amazon Rainforrest.
+# DIY_LoRa_ESP V2.0
+Self build ESP32 + LoRa device for environment monitoring on the Amazon Rainforrest.
 
 
-This device uses the ESP8266 D1 Mini microcontroller and the transciver 2AD66-LORAV2.
+This device uses the ESP32 C3 Mini microcontroller and the transciver 2AD66-LORAV2.
+The microcontroller was changed for SD card support.
 
-![Diagrama do Circuito](img/circuitR1.png)
+![Diagrama do Circuito](img/circuitR2.png)
